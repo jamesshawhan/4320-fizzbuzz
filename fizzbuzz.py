14f8380e1be8d8c71048bffeff174c9392028981
jamesshawhan <70238458+jamesshawhan@users.noreply.github.com>
2022-10-11 16:07:36 -0500

    only prints #s when not fizz, buzz, or fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,13 @@
 i = 1
 while i < 100:
-    print(i)
+    
     if (i % 3) == 0 and (i%5) == 0:
         print("fizzbuzz")
     elif (i%3) == 0:
         print("fizz")
     elif (i%5) == 0:
         print("buzz")
+    else:
+        print(i)
     
     i = i+1
