a6bf80229de230711bf48e82bfe1fc1a6c3b4f9d
jamesshawhan <70238458+jamesshawhan@users.noreply.github.com>
2022-10-11 16:13:26 -0500

    added comments

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,13 +1,12 @@
 i = 1
-while i < 100:
+while i <= 100:  #stops when we've hit 101
     
-    if (i % 3) == 0 and (i%5) == 0:
+    if (i % 3) == 0 and (i%5) == 0:          #if i is multiple of 3 and 5
         print("fizzbuzz")
-    elif (i%3) == 0:
+    elif (i%3) == 0:                 #if i is multiple of 3
         print("fizz")
-    elif (i%5) == 0:
+    elif (i%5) == 0:                #if i is multiple of 5
         print("buzz")
     else:
-        print(i)
-    
-    i = i+1
+        print(i)               # if i isn't a multiple of 3 or 5 it will print i
+    i = i+1              #iterating the loop
